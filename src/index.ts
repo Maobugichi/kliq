fe6870308d64792b6b69b5db4e6f3c30b2e273ee
Maobugichi <immansamuel846@gmail.com>
2026-05-06 09:56:22 +0100

    adjust cors wildcard

--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions)); 
+app.options(".*", cors(corsOptions)); 
 
 
 app.use("/api/payments/webhook", express.raw({ type: "application/json" }));
